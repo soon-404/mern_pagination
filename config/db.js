7556ca9efc965eeb42a9067f396d35e7175d77b3
soon-404 <tfpsoon@gmail.com>
2021-09-09 10:03:27 +0700

    fix env

--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useFindAndModify: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    // });
-    await mongoose.connect("mongodb://localhost:27017/mern_pagination");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // await mongoose.connect("mongodb://localhost:27017/mern_pagination");
     console.log("MongoDB Connection Success");
   } catch (error) {
     console.log("MongoDB Connection Failed");
